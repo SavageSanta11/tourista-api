2f53b40f5d173e992b046fc0404adb6a91852dbf
Aditi Kumaresan <adikumar13@publicisgroupe.net>
2023-07-28 16:49:38 +0000

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,9 @@
 import unittest
 import json
-from app import app
+from flask import Flask
+from flask.testing import FlaskClient
+from pymongo import MongoClient
+from app import app, collection
 
 class FlaskAppTest(unittest.TestCase):
 
@@ -8,70 +11,147 @@ class FlaskAppTest(unittest.TestCase):
         app.testing = True
         self.app = app.test_client()
 
+    def tearDown(self):
+        # Clear the collection after each test
+        collection.delete_many({})
+
     def test_hello_world(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode(), 'Hello, World!')
 
     def test_get_user_places(self):
-        phone_number = "whatsapp:+917204671691"
-        # Assuming the user with phone_number "1234567890" has places in the database
+        phone_number = "1234567890"
+        # Add a user with places to the collection
+        user = {
+            "phone_number": phone_number,
+            "places": ["Place 1", "Place 2", "Place 3"]
+        }
+        collection.insert_one(user)
+
         response = self.app.get(f'/api/users/{phone_number}/places')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
         self.assertIsInstance(data, list)
-
-    def test_get_user_places_not_found(self):
-        phone_number = "9876543210"
-        # Assuming the user with phone_number "9876543210" does not exist in the database
-        response = self.app.get(f'/api/users/{phone_number}/places')
-        self.assertEqual(response.status_code, 404)
-        data = json.loads(response.data.decode())
-        self.assertIn("message", data)
-        self.assertEqual(data["message"], "User not found")
+        self.assertEqual(data, user['places'])
 
     def test_update_user_places(self):
         phone_number = "1234567890"
+        places = ["Place 1", "Place 2", "Place 3"]
         data = {
-            "places": ["Place 1", "Place 2", "Place 3"]
+            "places": places
         }
+
+        response = self.app.patch(f'/api/users/{phone_number}/places', json=data)
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data.decode())
+        self.assertIn("message", data)
+        self.assertEqual(data["message"], "New user created successfully")
+
+        # Check if the user document is created in the collection
+        user = collection.find_one({"phone_number": phone_number})
+        self.assertIsNotNone(user)
+        self.assertEqual(user["places"], places)
+
+        # Update the places for the user
+        new_places = ["Place 4", "Place 5"]
+        data = {
+            "places": new_places
+        }
+
         response = self.app.patch(f'/api/users/{phone_number}/places', json=data)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
         self.assertIn("message", data)
         self.assertEqual(data["message"], "Places updated successfully")
 
+        # Check if the places are updated in the user document
+        user = collection.find_one({"phone_number": phone_number})
+        self.assertIsNotNone(user)
+        self.assertEqual(user["places"], new_places)
+
+    def test_get_user_location(self):
+        phone_number = "1234567890"
+        location = {
+            "lat": 123.456,
+            "long": 456.789,
+            "street_address": "123 Main St"
+        }
+        # Add a user with location to the collection
+        user = {
+            "phone_number": phone_number,
+            "location": location
+        }
+        collection.insert_one(user)
+
+        response = self.app.get(f'/api/users/{phone_number}/location')
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode())
+        self.assertEqual(data, location)
+
     def test_update_user_location(self):
         phone_number = "1234567890"
-        data = {
-            "location": {
-                "lat": 40.7128,
-                "long": -74.0060
-            }
+        location = {
+            "lat": 123.456,
+            "long": 456.789,
+            "street_address": "123 Main St"
         }
+        data = {
+            "location": location
+        }
+
+        response = self.app.patch(f'/api/users/{phone_number}/location', json=data)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data.decode())
+        self.assertIn("message", data)
+        self.assertEqual(data["message"], "New user created successfully")
+
+        # Check if the user document is created in the collection
+        user = collection.find_one({"phone_number": phone_number})
+        self.assertIsNotNone(user)
+        self.assertEqual(user["location"], location)
+
+        # Update the location for the user
+        new_location = {
+            "lat": 789.123,
+            "long": 321.654,
+            "street_address": "456 Elm St"
+        }
+        data = {
+            "location": new_location
+        }
+
         response = self.app.patch(f'/api/users/{phone_number}/location', json=data)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
         self.assertIn("message", data)
         self.assertEqual(data["message"], "Location updated successfully")
 
+        # Check if the location is updated in the user document
+        user = collection.find_one({"phone_number": phone_number})
+        self.assertIsNotNone(user)
+        self.assertEqual(user["location"], new_location)
+
     def test_remove_topmost_place(self):
         phone_number = "1234567890"
+        places = ["Place 1", "Place 2", "Place 3"]
+        # Add a user with places to the collection
+        user = {
+            "phone_number": phone_number,
+            "places": places
+        }
+        collection.insert_one(user)
+
         response = self.app.patch(f'/api/users/{phone_number}/places/remove')
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data.decode())
-        self.assertIn("message", data)
-        self.assertEqual(data["message"], "Topmost place removed successfully")
+        self.assertIn("place", data)
+        self.assertEqual(data["place"], places[0])
 
-    def test_remove_topmost_place_empty_places_list(self):
-        phone_number = "whatsapp:+18574924065"
-        # Assuming the user with phone_number "0987654321" exists, but the places list is empty
-        response = self.app.patch(f'/api/users/{phone_number}/places/remove')
-        self.assertEqual(response.status_code, 400)
-        data = json.loads(response.data.decode())
-        self.assertIn("message", data)
-        self.assertEqual(data["message"], "No places found for the user or places list is empty")
-
+        # Check if the topmost place is removed from the user document
+        user = collection.find_one({"phone_number": phone_number})
+        self.assertIsNotNone(user)
+        self.assertEqual(user["places"], places[1:])
 
 if __name__ == '__main__':
     unittest.main()
