1e88d50d5443e5ace8056ade4c84036723d6b1c3
Aditi Kumaresan <adikumar13@publicisgroupe.net>
2023-08-01 14:12:06 +0000

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,85 +1,97 @@
 import unittest
 from unittest.mock import patch
 from app import (
-app
+    get_user_places,
+    update_user_places,
+    get_user_location,
+    update_user_location,
+    remove_topmost_place,
+    remove_place_by_name,
+    get_chat_history,
+    update_chat_history,
+    get_user_interest,
+    store_user_interest,
+    app,
 )
-
-# Import mongomock's MongoClient
-from mongomock import MongoClient
+from pymongo.collection import Collection
+from mock import mock
 
 
 class TestAppEndpoints(unittest.TestCase):
     def setUp(self):
-        # Use mongomock's MongoClient for testing
-        self.mongo_client = MongoClient()
-        self.db = self.mongo_client["user_details"]
         app.config["TESTING"] = True
         self.app = app.test_client()
-
-    def tearDown(self):
-        # Clear the database after each test
-        self.mongo_client.drop_database("user_details")
 
     def test_get_user_places_existing_user(self):
         # Arrange
         phone_number = "1234567890"
         test_places = [{"title": "Place 1"}, {"title": "Place 2"}]
-        self.db["info"].insert_one({"phone_number": phone_number, "places": test_places})
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = {"phone_number": phone_number, "places": test_places}
 
-        # Act
-        response = self.app.get(f"/api/users/{phone_number}/places")
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Act
+            response = self.app.get(f"/api/users/{phone_number}/places")
+            data = response.get_json()
+            status_code = response.status_code
 
-        # Assert
-        self.assertEqual(status_code, 200)
-        self.assertEqual(data, test_places)
+            # Assert
+            self.assertEqual(status_code, 200)
+            self.assertEqual(data, test_places)
 
     def test_get_user_places_user_not_found(self):
         # Arrange
         phone_number = "1234567890"
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = None
 
-        # Act
-        response = self.app.get(f"/api/users/{phone_number}/places")
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Act
+            response = self.app.get(f"/api/users/{phone_number}/places")
+            data = response.get_json()
+            status_code = response.status_code
 
-        # Assert
-        self.assertEqual(status_code, 404)
-        self.assertEqual(data, {"message": "User not found"})
+            # Assert
+            self.assertEqual(status_code, 404)
+            self.assertEqual(data, {"message": "User not found"})
 
     def test_update_user_places_existing_user(self):
         # Arrange
         phone_number = "1234567890"
         test_data = {"places": [{"title": "Place 1"}, {"title": "Place 2"}]}
-        self.db["info"].insert_one({"phone_number": phone_number})
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = {"phone_number": phone_number}
 
-        # Act
-        response = self.app.patch(
-            f"/api/users/{phone_number}/places", json=test_data
-        )
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Use the test_request_context to mock the Flask app
+            with app.test_request_context(json=test_data):
+                # Act
+                response = update_user_places(phone_number)
+                status_code = response[1]
 
-        # Assert
-        self.assertEqual(status_code, 200)
-        self.assertEqual(data, {"message": "Places updated successfully"})
+                # Assert
+                self.assertEqual(status_code, 200)
 
     def test_update_user_places_new_user(self):
         # Arrange
         phone_number = "1234567890"
         test_data = {"places": [{"title": "Place 1"}, {"title": "Place 2"}]}
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = None
 
-        # Act
-        response = self.app.patch(
-            f"/api/users/{phone_number}/places", json=test_data
-        )
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Use the test_request_context to mock the Flask app
+            with app.test_request_context(json=test_data):
+                # Act
+                response = update_user_places(phone_number)
+                status_code = response[1]
 
-        # Assert
-        self.assertEqual(status_code, 201)
-        self.assertEqual(data, {"message": "New user created successfully"})
+                # Assert
+                self.assertEqual(status_code, 201)
 
     def test_get_user_location_existing_user_with_location(self):
         # Arrange
@@ -89,34 +101,38 @@ class TestAppEndpoints(unittest.TestCase):
             "long": -74.0060,
             "street_address": "New York City, NY",
         }
-        self.db["info"].insert_one(
-            {"phone_number": phone_number, "location": test_location}
-        )
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = {"phone_number": phone_number, "location": test_location}
 
-        # Act
-        response = self.app.get(f"/api/users/{phone_number}/location")
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Act
+            response = self.app.get(f"/api/users/{phone_number}/location")
+            data = response.get_json()
+            status_code = response.status_code
 
-        # Assert
-        self.assertEqual(status_code, 200)
-        self.assertEqual(data, test_location)
+            # Assert
+            self.assertEqual(status_code, 200)
+            self.assertEqual(data, test_location)
 
     def test_get_user_location_existing_user_without_location(self):
         # Arrange
         phone_number = "1234567890"
-        self.db["info"].insert_one({"phone_number": phone_number})
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = {"phone_number": phone_number}
 
-        # Act
-        response = self.app.get(f"/api/users/{phone_number}/location")
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Act
+            response = self.app.get(f"/api/users/{phone_number}/location")
+            data = response.get_json()
+            status_code = response.status_code
 
-        # Assert
-        self.assertEqual(status_code, 404)
-        self.assertEqual(
-            data, {"message": "User not found or location not available"}
-        )
+            # Assert
+            self.assertEqual(status_code, 404)
+            self.assertEqual(
+                data, {"message": "User not found or location not available"}
+            )
 
     def test_update_user_location_existing_user(self):
         # Arrange
@@ -128,18 +144,19 @@ class TestAppEndpoints(unittest.TestCase):
                 "street_address": "New York City, NY",
             }
         }
-        self.db["info"].insert_one({"phone_number": phone_number})
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = {"phone_number": phone_number}
 
-        # Act
-        response = self.app.patch(
-            f"/api/users/{phone_number}/location", json=test_data
-        )
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Use the test_request_context to mock the Flask app
+            with app.test_request_context(json=test_data):
+                # Act
+                response = update_user_location(phone_number)
+                status_code = response[1]
 
-        # Assert
-        self.assertEqual(status_code, 200)
-        self.assertEqual(data, {"message": "Location updated successfully"})
+                # Assert
+                self.assertEqual(status_code, 200)
 
     def test_update_user_location_new_user(self):
         # Arrange
@@ -151,17 +168,19 @@ class TestAppEndpoints(unittest.TestCase):
                 "street_address": "New York City, NY",
             }
         }
+        mock_collection = mock.create_autospec(Collection)
+        mock_collection.find_one.return_value = None
 
-        # Act
-        response = self.app.patch(
-            f"/api/users/{phone_number}/location", json=test_data
-        )
-        data = response.get_json()
-        status_code = response.status_code
+        # Mock the MongoDB collection
+        with patch("app.collection", mock_collection):
+            # Use the test_request_context to mock the Flask app
+            with app.test_request_context(json=test_data):
+                # Act
+                response = update_user_location(phone_number)
+                status_code = response[1]
 
-        # Assert
-        self.assertEqual(status_code, 200)
-        self.assertEqual(data, {"message": "New user created successfully"})
+                # Assert
+                self.assertEqual(status_code, 200)
 
     # Continue with other tests for remaining endpoints...
 
